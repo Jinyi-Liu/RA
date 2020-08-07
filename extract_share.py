39fe6b1f90a5cb57703d435e0762cc7fd70c17b2
Jinyi <jinyi.liu99@gmail.com>
2020-08-07 22:26:16 +0800

    Update extract_share: find tag

--- extract_share.py
+++ extract_share.py
@@ -7,12 +7,28 @@ html_path = './txt2html_files'
 html_files = os.listdir(html_path)
 html_files.sort()
 
+keyword='outstanding'
+
+def get_paragraph_with_keyword(soup, tag, keyword=keyword):
+    tags = soup.find_all(tag)
+    _tags = [item for item in tags if keyword in item.get_text()]
+    tag_contents = []
+    for tag in _tags:
+        temp = []
+        for child_tag in tag:
+            if type(child_tag) == bs4.element.NavigableString:
+                temp.append(child_tag)
+            else:
+                temp.append(child_tag.get_text())
+        tag_contents.extend(temp)
+    return ' '.join(''.join(tag_contents).split())
+
 '''
 Extract the outstanding shares from a given html file
 '''
 pre_name = 16160
 files = [item for item in html_files if str(pre_name)+'_' in item]
-file='16160_1.html'
+file='16160_2.html'
 for file in files:
     data = open(html_path + '/' + file)
     handle = data.read()
@@ -22,6 +38,7 @@ for file in files:
     Solved:
     tag <p> with something like <font>****</font> in it.
     Example: 16160_1.html
+    '''
     '''
     p = soup.find_all('p')
     _p = [item for item in p if 'outstanding' in item.get_text()]
@@ -35,5 +52,19 @@ for file in files:
                 temp.append(child_tag.get_text())
         p_content.extend(temp)
     p_text = ' '.join(''.join(p_content).split())
-    re_num = re.compile('\d{1,3}(?:\,\d{3})+(?:\.\d{2})?')
+    '''
+    p_text = get_paragraph_with_keyword(soup,'p','outstanding')
 
+    re_num = re.compile('\d{1,3}(?:\,\d{3})+(?:\.\d{2})?')
+    test = re_num.findall(p_text)
+
+    div = soup.find_all('div')
+    _div = [item for item in div if 'outstanding' in item.get_text()]
+
+
+
+
+t = get_paragraph_with_keyword(soup, 'div',keyword)
+re_num.findall(t)
+    print(file)
+    print(test)
