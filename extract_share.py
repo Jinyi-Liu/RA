eb8575c4502d96c20ac57b3b03125aca58030cc3
Jinyi <jinyi.liu99@gmail.com>
2020-08-07 16:40:54 +0800

    Update extract_share: for outstanding in tag <p>

--- extract_share.py
+++ extract_share.py
@@ -34,32 +34,6 @@ for file in files:
             else:
                 temp.append(child_tag.get_text())
         p_content.extend(temp)
-    '''
-    f_test = open('test.txt', 'w')
-    for child in p_content:
-        p_text = ' '.join(''.join(child).split())
-        f_test.write(p_text)
-        f_test.write('\n\r')
-    f_test.close()
     p_text = ' '.join(''.join(p_content).split())
-    f_test = open('test.txt','w')
-    f_test.write(p_text)
-    f_test.close()
-    '''
-    p_text = ' '.join(''.join(p_content).split())
-    ###
-    #reg = re.compile('.*outstanding*.')
-    #tag = soup.find_all(text=reg)
-    #pre = [' '.join(item.split()) for item in tag]
     re_num = re.compile('\d{1,3}(?:\,\d{3})+(?:\.\d{2})?')
-    print(file)
-    print(re_num.findall(p_text)[:8])
-    #num = [re_num.findall(tag_item) for tag_item in tag]
-    #num = [item for item in num if len(item)>5]
-    '''
-    num_list = []
-    for item in num:
-        num_list.extend(item)
-    print(file)
-    print(num_list)
-    '''
+
